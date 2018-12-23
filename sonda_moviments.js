21faf8ca97160d3c36baf2087ca04acc7c3f67dc
Marco Aurélio <marcofilho_1@hotmail.com>
2018-12-23 10:29:01 -0200

    Criando lógica para movimentar posição do eixo da sonda na malha do planalto.

--- sonda_moviments.js
+++ sonda_moviments.js
@@ -1,10 +1,15 @@
+var mapMoviments = []
 var mapDirections = []
 var starDirections = []
 
-var loadMaps = () => {
-    mapDirections['M'] = 1;
+var loadMaps = function() {
     mapDirections['R'] = 90;
     mapDirections['L'] = -90;
+
+    mapMoviments['EM'] = '1,0';
+    mapMoviments['NM'] = '0,1';
+    mapMoviments['SM'] = '0,-1';
+    mapMoviments['WM'] = '-1,0';
 
     starDirections['N'] = 0;
     starDirections['E'] = 90;
@@ -12,32 +17,38 @@ var loadMaps = () => {
     starDirections['W'] = 270;
 }
 
-var startMoviment = function(sonda) {
+var getValueDirection = function(sonda, moviment) {
+    var starDirection = starDirections[sonda.direction] + mapDirections[moviment]
+
+    if(starDirection == 360) {
+        starDirection = 0
+    }
+
+    if(starDirection < 0) {
+        starDirection = 270
+    }
+
+    if(starDirection > 360) {
+        starDirection = 90
+    }
+
+    return starDirection
+}
+
+var startMoviment = (sonda) => {
     sonda.moviments_sonda.split('').map(function(moviment) {
-        if(mapDirections[moviment]) {
-            if((moviment == 'R' || moviment == 'L')) {
-                var starDirection = starDirections[sonda.direction] + mapDirections[moviment]
+        if((moviment == 'R' || moviment == 'L') && mapDirections[moviment]) {
+            var starDirection = getValueDirection(sonda, moviment)
+            sonda.direction = Object.keys(starDirections).filter((k) => {
+                return starDirections[k] == starDirection
+            }).shift()
+        }
 
-                if(starDirection == 360) {
-                    starDirection = 0;
-                }
-
-                if(starDirection < 0) {
-                    starDirection = 270;
-                }
-
-                if(starDirection > 360) {
-                    starDirection = 90;
-                }
-
-                sonda.direction = Object.keys(starDirections).filter((k) => {
-                    return starDirections[k] == starDirection;
-                }).shift()
-            }
-
-            if(moviment == 'M') {
-    
-            }
+        var coordMoviment = mapMoviments[sonda.direction + moviment]
+        if(moviment == 'M' && coordMoviment) {
+            coordMoviment = coordMoviment.split(',')
+            sonda.x += parseInt(coordMoviment[0])
+            sonda.y += parseInt(coordMoviment[1])
         }
     })
 }
